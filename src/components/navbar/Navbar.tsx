35d3c93142090087ff11939df672dd887ed59776
Bao Nguyen <bao.nguyen@insta.fi>
2024-01-03 13:19:08 +0200

    navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,54 @@
+import { Close, Menu } from "@mui/icons-material";
 import { useState } from "react";
 import { navLinks } from "../../constants";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  // Cái này có thể là CSS hoặc là state? Nhưng khả năng cao là CSS
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <div className={classes.navbar}>
       {/* Logo */}
       <span>Bao Nguyen</span>
 
-      {/* Navlink for normal screen */}
-      {isMobile ?? (
-        <ul>
+      {
+        <ul className={classes.navList}>
           {navLinks.map((link) => (
             <li>
               <a>{link.name}</a>
             </li>
           ))}
         </ul>
-      )}
+      }
+
+      {
+        <div className={classes.iconWrapper}>
+          {isMenuOpen ? (
+            <Close onClick={() => setIsMenuOpen(false)} />
+          ) : (
+            <Menu onClick={() => setIsMenuOpen(true)} />
+          )}
+        </div>
+      }
+
+      {/* Navlink for mobile */}
+      {
+        <div
+          className={`${classes.mobileMenu} ${
+            isMenuOpen ? classes.menuOpen : ""
+          }`}
+        >
+          <ul className={`${classes.mobileNavList}`}>
+            {navLinks.map((link) => (
+              <li key={link.name}>
+                <a>{link.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      }
     </div>
   );
 };
